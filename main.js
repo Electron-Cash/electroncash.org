b0eb75ac2f7b373a25c056c3cff40b07431010cc
Daniel Wu <wuonlabs@gmail.com>
2018-05-27 14:32:25 -0400

    initial

---
+++ main.js
@@ -0,0 +1,83 @@
+window.onload = function () {
+
+	$("#download-button").click(function() {
+		$('html,body').animate({
+			scrollTop: $("#download").offset().top},
+			'slow');
+	});
+
+	$("#faq-button").click(function() {
+		$('html,body').animate({
+			scrollTop: $("#faq").offset().top},
+			'slow');
+	});
+
+	$("#benefits-button").click(function() {
+		$('html,body').animate({
+			scrollTop: $("#benefits").offset().top},
+			'slow');
+	});
+
+	$("#download-button2").click(function() {
+		$('html,body').animate({
+			scrollTop: $("#download").offset().top},
+			'slow');
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+
+	$("#faq-button2").click(function() {
+		$('html,body').animate({
+			scrollTop: $("#faq").offset().top},
+			'slow');
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+
+	$("#benefits-button2").click(function() {
+		$('html,body').animate({
+			scrollTop: $("#benefits").offset().top},
+			'slow');
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+
+	$("#download-button3").click(function() {
+		$('html,body').animate({
+			scrollTop: $("#download").offset().top},
+			'slow');
+	});
+
+	$(window).scroll(function(){
+	    var scrollTop = $(window).scrollTop();
+	    if (scrollTop > 75) {
+	        $('.erasable-header').addClass('shrink');
+	    } else {
+	        $('.erasable-header').removeClass('shrink');
+	    }
+  	});
+
+  	$('.hamburger-shell').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	});
+	$('.bg-cover').click(function(){
+		$('#menu').slideToggle(300);
+		$('.top').toggleClass('rotate');
+		$('.middle').toggleClass('rotate-back');
+		$('.menu-name').toggleClass('bump');
+		$('.bg-cover').toggleClass('reveal');
+	})
+}
